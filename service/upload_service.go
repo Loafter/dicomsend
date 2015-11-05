a6f8415adfe404319cb411cb56723d982502eeba
Loafter <mopper@yandex.ru>
2015-11-05 19:48:16 +0300

    add test

--- service/upload_service.go
+++ service/upload_service.go
@@ -5,13 +5,22 @@ import (
 	"io/ioutil"
 	"log"
 	"encoding/base64"
-	"os"
-	"io"
+	/*"os"
+	"io"*/
+	"crypto/rand"
+	"fmt"
 )
 type UpSrv struct {
 
 }
 const  htmlData=""
+const FlushDiskSize = 1024 * 1024
+
+func genUid() string {
+	b := make([]byte, 16)
+	rand.Read(b)
+	return fmt.Sprintf("%X-%X-%X-%X-%X", b[0:4], b[4:6], b[6:8], b[8:10], b[10:])
+}
 func (srv *UpSrv) Start(listenPort int) error {
 	http.HandleFunc("/", srv.Redirect)
 	http.HandleFunc("/index.html", srv.index)
@@ -24,6 +33,7 @@ func (srv *UpSrv) Start(listenPort int) error {
 func (srv *UpSrv) Redirect(responseWriter http.ResponseWriter, request *http.Request) {
 	http.Redirect(responseWriter, request, "/index.html", 301)
 }
+
 
 func (srv *UpSrv) index(rwr http.ResponseWriter, req *http.Request) {
 	rwr.Header().Set("Content-Type: text/html", "*")
@@ -38,20 +48,34 @@ func (srv *UpSrv) index(rwr http.ResponseWriter, req *http.Request) {
 }
 
 func (srv *UpSrv)uploadDicom(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
 	log.Println("try upload")
-	err := r.ParseMultipartForm(200000) // grab the multipart form
-	if err != nil {
-		log.Println(w,err)
-		return
+	rd,err :=r.MultipartReader()
+	if err!=nil{
+		log.Println("error:"+ err.Error())
+	}
+	//buffer := make([]byte, FlushDiskSize, FlushDiskSize)
+	for p,err:=rd.NextPart();err==nil;p,err=rd.NextPart(){
+
+		/*c,er:=p.Read(buffer){
+			if er!=nil{
+				return
+			}
+		}*/
+		log.Println(p.)
+
 	}
 
 
+	/* err != nil {
+		log.Println(w,err)
+		return
+	}
 	formdata := r.MultipartForm  // ok, no problem so far, read the Form data
-
 	//get the *fileheaders
 	files := formdata.File["multiplefiles"]  // grab the filenames
-
 	for i, _ := range files {  // loop through the files one by one
+		log.Println(i)
 		file, err := files[i].Open()
 		defer file.Close()
 		if err != nil {
@@ -76,6 +100,6 @@ func (srv *UpSrv)uploadDicom(w http.ResponseWriter, r *http.Request) {
 		log.Println(w,"Files uploaded successfully : ")
 		log.Println(w, files[i].Filename + "\n")
 
-	}
+	}*/
 }
 
